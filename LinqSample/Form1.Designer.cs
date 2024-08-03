538b85c9625464d916c19cd47f6746c8aa693ea1
chegeni-0608 <mmoa80610@gmail.com>
2024-08-03 14:54:27 +0330

    2024-08-03form1methodAddToListBox

--- LinqSample/Form1.Designer.cs
+++ LinqSample/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.btnLinqSample01 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnSample02 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnLinqSample01
@@ -41,30 +42,42 @@
             this.btnLinqSample01.TabIndex = 0;
             this.btnLinqSample01.Text = "LinqSample01";
             this.btnLinqSample01.UseVisualStyleBackColor = true;
+            this.btnLinqSample01.Click += new System.EventHandler(this.btnLinqSample01_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(595, 92);
+            this.listBox1.Location = new System.Drawing.Point(595, 52);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(193, 344);
+            this.listBox1.Size = new System.Drawing.Size(193, 384);
             this.listBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(591, 54);
+            this.label1.Location = new System.Drawing.Point(591, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(63, 20);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Result :";
+            // 
+            // btnSample02
+            // 
+            this.btnSample02.Location = new System.Drawing.Point(47, 97);
+            this.btnSample02.Name = "btnSample02";
+            this.btnSample02.Size = new System.Drawing.Size(139, 42);
+            this.btnSample02.TabIndex = 3;
+            this.btnSample02.Text = "LinqSample02";
+            this.btnSample02.UseVisualStyleBackColor = true;
+            this.btnSample02.Click += new System.EventHandler(this.btnSample02_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSample02);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnLinqSample01);
@@ -80,6 +93,7 @@
         private System.Windows.Forms.Button btnLinqSample01;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSample02;
     }
 }
 
--- LinqSample/Form1.cs
+++ LinqSample/Form1.cs
@@ -12,9 +12,48 @@ namespace LinqSample
 {
     public partial class Form1 : Form
     {
+        int[]  _numbers = {14,58,52,475,11,20,14,47,58 };
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnLinqSample01_Click(object sender, EventArgs e)
+        {
+            var result = from n in _numbers
+                         where n % 2 != 0
+                         select n;
+            AddToListBox(result.ToArray());
+
+            //foreach (var item in result)
+            //{
+            // listBox1.Items.Add(item);
+            //}
+        }
+
+        private void btnSample02_Click(object sender, EventArgs e)
+        {
+            var result = from q in _numbers
+                         where q % 2 ==0
+                         orderby q descending
+                         select q;
+           
+            AddToListBox(result.ToArray());
+
+
+            //foreach (var item in result)
+            //{
+            //    listBox1.Items.Add(item);
+            //}
+
+        }
+        private void AddToListBox(int[] items)
+        {   
+         listBox1.Items.Clear();
+            foreach (var item in items)
+            {
+                listBox1.Items.Add(item);
+            }
         }
     }
 }
